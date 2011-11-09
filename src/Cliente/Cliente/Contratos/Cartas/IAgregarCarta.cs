cabb28ef1b97585e027931eb967cecd232405cb8
sondy99@gmail.com <sondy99@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2011-11-09 21:16:06 +0000

    Servidor de carta listo, y contratos de carta

---
+++ src/Cliente/Cliente/Contratos/Cartas/IAgregarCarta.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Trascend.Bolet.Cliente.Contratos.Cartas
+{
+    interface IAgregarCarta : IPaginaBase
+    {
+        object Carta { get; set; }
+
+        void Mensaje(string mensaje);
+
+        object Asociado { get; set; }
+
+        object Asociados { get; set; }
+
+        object Receptor { get; set; }
+
+        object Receptores { get; set; }
+
+        object Remitente { get; set; }
+
+        object Remitentes { get; set; }
+
+        object Responsable { get; set; }
+
+        object Responsables { get; set; }
+
+        object Resumen { get; set; }
+
+        object Resumenes { get; set; }
+
+        object Departamento { get; set; }
+
+        object Departamentos { get; set; }
+
+        object Persona { get; set; }
+
+        object Personas { get; set; }
+
+        object Medio { get; set; }
+
+        object Medios { get; set; }
+
+        object Anexo { get; set; }
+
+        object Anexos { get; set; }
+
+        object AnexoCarta { get; set; }
+
+        object AnexosCarta { get; set; }
+
+        object AnexoConfirmacion { get; set; }
+
+        object AnexosConfirmacion { get; set; }
+
+        object AnexoConfirmacionCarta { get; set; }
+
+        object AnexosConfirmacionCarta { get; set; }
+
+    }
+}
