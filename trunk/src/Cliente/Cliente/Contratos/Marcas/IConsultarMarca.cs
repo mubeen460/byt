c711d7a6df3f6f4ee31c8c414f7e16c00b96be01
wsotobianco@gmail.com <wsotobianco@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2011-12-12 19:39:35 +0000

    Arreglos a la IConsultarMarca.cs e implementacion por la mitad en ConsultarMarca

--- trunk/src/Cliente/Cliente/Contratos/Marcas/IConsultarMarca.cs
+++ trunk/src/Cliente/Cliente/Contratos/Marcas/IConsultarMarca.cs
@@ -17,23 +17,41 @@ namespace Trascend.Bolet.Cliente.Contratos.Marcas
 
         object Asociado { get; set; }
 
+        object PoderesSolicitud { get; set; }
+
         object PoderSolicitud { get; set; }
+
+        object PoderesDatos { get; set; }
 
         object PoderDatos { get; set; }
 
+        object Agentes { get; set; }
+
         object Agente { get; set; }
+
+        object BoletinesOrdenPublicacion { get; set; }
 
         object BoletinOrdenPublicacion { get; set; }
 
+        object BoletinesPublicacion { get; set; }
+
         object BoletinPublicacion { get; set; }
 
+        object BoletinesConcesion { get; set; }
+
         object BoletinConcesion { get; set; }
+
+        object Servicios { get; set; }
 
         object Servicio { get; set; }
 
         object Detalle { get; set; }
 
+        object Detalles { get; set; }
+
         object Condiciones { get; set; }
+
+        object Condicion { get; set; }
 
         void Mensaje(string mensaje);
     }
