ea375d04538ea416b18c32e90a2b53c71aa485ba
wsotobianco@gmail.com <wsotobianco@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2011-12-12 18:31:24 +0000

    Interfaz IConsultarMarca.cs

---
+++ src/Cliente/Cliente/Contratos/Marcas/IConsultarMarca.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Controls;
+using Trascend.Bolet.Cliente.Ayuda;
+
+namespace Trascend.Bolet.Cliente.Contratos.Marcas
+{
+    interface IConsultarMarca : IPaginaBase
+    {
+        object Marca { get; set; }
+
+        string IdAsociadoFiltrar { get; }
+
+        string NombreAsociadoFiltrar { get; }
+
+        string NombreAsociado { get; set; }
+
+        object Asociados { get; set; }
+
+        object Asociado { get; set; }
+
+        object PoderSolicitud { get; set; }
+
+        object PoderDatos { get; set; }
+
+        object Agente { get; set; }
+
+        object BoletinOrdenPublicacion { get; set; }
+
+        object BoletinPublicacion { get; set; }
+
+        object BoletinConcesion { get; set; }
+
+        object Servicio { get; set; }
+
+        object Detalle { get; set; }
+
+        object Condiciones { get; set; }
+
+        void Mensaje(string mensaje);
+    }
+}
