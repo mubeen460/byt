5d756e918d5be4df2b6cba11edb7d8102b2ceb57
wsotobianco@gmail.com <wsotobianco@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-02-04 00:07:05 +0000

    Arreglos en cesion, agregado GestionarCesion agregadas etiquetas para cesion actualizado PresentadorConsultarCesiones. Falta agregar varios campos a la ventana de detalle de GestionarCesion

---
+++ trunk/src/Cliente/Cliente/Contratos/Traspasos/Cesiones/IGestionarCesion.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Controls;
+using Trascend.Bolet.Cliente.Ayuda;
+
+namespace Trascend.Bolet.Cliente.Contratos.Traspasos.Cesiones
+{
+    interface IGestionarCesion : IPaginaBase
+    {
+        object Cesion { get; set; }
+
+        string IdAsociadoFiltrar { get; }
+
+        string NombreAsociadoFiltrar { get; }
+
+        string IdInteresadoEntreFiltrar { get; }
+
+        string NombreInteresadoEntreFiltrar { get; }
+
+        string IdInteresadoSobrevivienteFiltrar { get; }
+
+        string NombreInteresadoSobrevivienteFiltrar { get; }
+
+        string IdAgenteFiltrar { get; }
+
+        string NombreAgenteFiltrar { get; }       
+        
+        //ListView Marcas
+
+        string IdMarcaFiltrar { get; }
+
+        string NombreMarcaFiltrar { get; }
+
+        object MarcasFiltradas { get; set; }
+
+        object MarcaFiltrada { get; set; }
+
+        //-----------
+
+        bool HabilitarCampos { set; }
+
+        string Region { get; set; }
+
+        string TextoBotonModificar { get; set; }
+
+        string NombreMarca { set; }
+
+        object Marca { get; set; }
+
+        GridViewColumnHeader CurSortCol { get; set; }
+
+        SortAdorner CurAdorner { get; set; }
+    }
+}
