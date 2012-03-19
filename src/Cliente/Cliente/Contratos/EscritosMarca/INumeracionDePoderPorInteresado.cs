6af47577b5dd6c1245640eec9cac5f4eabdf2991
danijose24@gmail.com <danijose24@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-03-19 21:59:53 +0000

    Implementacion de escrito Numeracion de poder por interesado

---
+++ src/Cliente/Cliente/Contratos/EscritosMarca/INumeracionDePoderPorInteresado.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Windows.Controls;
+using Trascend.Bolet.Cliente.Ayuda;
+namespace Trascend.Bolet.Cliente.Contratos.EscritosMarca
+{
+    interface INumeracionDePoderPorInteresado : IPaginaBase
+    {
+        object Escrito { get; set; }
+
+        #region Agente
+
+        object Agente { get; set; }
+
+        string NombreAgente { set; }
+
+        string IdAgenteFiltrar { get; }
+
+        string NombreAgenteFiltrar { get; }
+
+        object AgentesFiltrados { get; set; }
+
+        object AgenteFiltrado { get; set; }
+
+        #endregion
+
+        #region Interesado
+
+        object Interesado { get; set; }
+
+        string NombreInteresado { set; }
+
+        string IdInteresadoFiltrar { get; }
+
+        string NombreInteresadoFiltrar { get; }
+
+        object InteresadosFiltrados { get; set; }
+
+        object InteresadoFiltrado { get; set; }
+
+        #endregion
+
+        #region Interesados Agregados
+
+        object InteresadosAgregados { get; set; }
+
+        object InteresadoAgregado { get; set; }
+
+        #endregion
+
+        void MensajeAlerta(string mensaje);
+
+        GridViewColumnHeader CurSortCol { get; set; }
+
+        SortAdorner CurAdorner { get; set; }
+
+        string BotonModificar { get; set; }
+
+        bool HabilitarCampos { set; }
+    }
+}
