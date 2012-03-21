e48f0d380d1697909800b2e412cbffb17ca1f622
hachuelsimon@gmail.com <hachuelsimon@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-03-21 22:47:25 +0000

    Implementacion Consignacion de Busqueda

---
+++ src/Cliente/Cliente/Contratos/EscritosMarca/IConsignacionDeBusqueda.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Windows.Controls;
+using Trascend.Bolet.Cliente.Ayuda;
+namespace Trascend.Bolet.Cliente.Contratos.EscritosMarca
+{
+    interface IConsignacionDeBusqueda : IPaginaBase
+    {
+        object Escrito { get; set; }
+
+        #region Agente
+
+        object Agente { get; set; }
+
+        string NombreAgente { set; }
+
+        string IdAgenteFiltrar { get; }
+
+        string NombreAgenteFiltrar { get; }
+
+        object AgentesFiltrados { get; set; }
+
+        object AgenteFiltrado { get; set; }
+
+        #endregion
+
+        #region Marca
+
+        object Marca { get; set; }
+
+        string NombreMarca { set; }
+
+        string IdMarcaFiltrar { get; }
+
+        string NombreMarcaFiltrar { get; }
+
+        object MarcasFiltrados { get; set; }
+
+        object MarcaFiltrado { get; set; }
+
+        #endregion
+
+        #region Marcas Busqueda
+
+        object MarcasBusqueda { get; set; }
+
+        object MarcaBusquedaSeleccionada { get; set; }
+
+        #endregion
+
+        void MensajeAlerta(string mensaje);
+
+        GridViewColumnHeader CurSortCol { get; set; }
+
+        SortAdorner CurAdorner { get; set; }
+
+        string BotonModificar { get; set; }
+
+        CheckBox ChkMixto { get; set; }
+
+        CheckBox ChkPalabra { get; set; }
+
+        CheckBox ChkDiseño { get; set; }
+
+        void AsignarCheckbox();
+
+        bool HabilitarCampos { set; }
+    }
+}
