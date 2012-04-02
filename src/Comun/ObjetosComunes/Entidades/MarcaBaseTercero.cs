c63727050719408e261cd14a033f8897db9a195a
hachuelsimon@gmail.com <hachuelsimon@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-04-02 02:18:43 +0000

    arreglo del mapeo de MarcaTercero, implementacion de agregar marcatercero y marcabase

--- src/Comun/ObjetosComunes/Entidades/MarcaBaseTercero.cs
+++ src/Comun/ObjetosComunes/Entidades/MarcaBaseTercero.cs
@@ -39,6 +39,43 @@ namespace Trascend.Bolet.ObjetosComunes.Entidades
 
         #region Propiedades
 
+
+        /// <summary>
+        /// Sobreescritura del Método Equals debido a que la clase tiene id compuesto
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            var t = obj as MarcaBaseTercero;
+            if (t == null)
+                return false;
+            if ((Id == (t.Id)) && (this.Id == (t.Id)))
+                return true;
+            return false;
+
+        }
+
+        /// <summary>
+        /// Sobreescritura del Método GetHashCode debido a que la clase tiene id compuesto
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Sobreescritura del Método ToString debido a que la clase tiene id compuesto
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
         /// <summary>
         /// Propiedad que asigna u obtiene el Id
         /// </summary>
