43270015dada7d47d08f5b31fab5561892768f6d
wsotobianco@gmail.com <wsotobianco@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-04-10 16:14:56 +0000

    Arreglos generales en EjecutarComandoDeConsola

--- trunk/src/Cliente/Cliente/Presentadores/PresentadorBase.cs
+++ trunk/src/Cliente/Cliente/Presentadores/PresentadorBase.cs
@@ -1602,5 +1602,29 @@ namespace Trascend.Bolet.Cliente.Presentadores
             }
             return retorno;
         }
+
+        /// <summary>
+        /// Ejecuta un comando en consola sin abrirla
+        /// </summary>
+        /// <param name="comando">Comando que se quiere ejecutar en consola</param>
+        public void EjecutarComandoDeConsola(string comando, string accion)
+        {
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", comando);
+
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            // Do not create the black window.
+            procStartInfo.CreateNoWindow = true;
+            // Now we create a process, assign its ProcessStartInfo and start it
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            // Get the output into a string
+            string result = proc.StandardOutput.ReadToEnd();
+
+            #region Debug
+            logger.Debug("Resultado del comando de consola '" + accion + "': " + result);
+            #endregion
+        }
     }
 }
