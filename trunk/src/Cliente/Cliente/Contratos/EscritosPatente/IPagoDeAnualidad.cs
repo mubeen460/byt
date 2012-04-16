0b3b7e78b31adfb25c12e9bacae1311962665442
hachuelsimon@gmail.com <hachuelsimon@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-04-16 21:37:29 +0000

    Se Agregan los escritos de ContestacionAOposicion PagoDeAnualidad y ReconsideracionPrioridadExtinguida

---
+++ trunk/src/Cliente/Cliente/Contratos/EscritosPatente/IPagoDeAnualidad.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Windows.Controls;
+using Trascend.Bolet.Cliente.Ayuda;
+namespace Trascend.Bolet.Cliente.Contratos.EscritosPatente
+{
+    interface IPagoDeAnualidad : IPaginaBase
+    {
+        object Escrito { get; set; }
+
+        #region Agente
+
+        object Agente { get; set; }
+
+        string NombreAgente { set; }
+
+        string IdAgenteFiltrar { get; }
+
+        string NombreAgenteFiltrar { get; }
+
+        object AgentesFiltrados { get; set; }
+
+        object AgenteFiltrado { get; set; }
+
+        #endregion
+
+        #region Patente
+
+        object Patente { get; set; }
+
+        string NombrePatente { set; }
+
+        string IdPatenteFiltrar { get; }
+
+        string NombrePatenteFiltrar { get; }
+
+        object PatentesFiltrados { get; set; }
+
+        object PatenteFiltrado { get; set; }
+
+        #endregion
+
+        #region Patentes Agregadas
+
+        object PatentesAgregadas { get; set; }
+
+        object PatenteAgregada { get; set; }
+
+        #endregion
+
+        string Fecha { get; }
+
+        void MensajeAlerta(string mensaje);
+
+        GridViewColumnHeader CurSortCol { get; set; }
+
+        SortAdorner CurAdorner { get; set; }
+
+        string BotonModificar { get; set; }
+
+        bool HabilitarCampos { set; }
+    }
+}
