a23293ccd006d7c175409a0987b3087cc90e4fd1
hachuelsimon@gmail.com <hachuelsimon@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-05-29 18:54:11 +0000

    se agregaron comentarios

--- src/Servidor/Transaccional/AccesoDatos/Contrato/IDaoMarcaBaseTercero.cs
+++ src/Servidor/Transaccional/AccesoDatos/Contrato/IDaoMarcaBaseTercero.cs
@@ -5,12 +5,29 @@ namespace Trascend.Bolet.AccesoDatos.Contrato
 {
     public interface IDaoMarcaBaseTercero : IDaoBase<MarcaBaseTercero, int>
     {
+
+        /// <summary>
+        /// Metodo que consulta las MarcaBaseTercero dado unos parametros
+        /// </summary>
+        /// <param name="marcaBaseTercero">MarcaBaseTercero </param>
+        /// <returns>Todas las MarcaBaseTercero solicitados</returns>
         IList<MarcaBaseTercero> ObtenerMarcaBaseTerceroFiltro(MarcaBaseTercero marcaBaseTercero);
 
+
+        /// <summary>
+        /// metodo que obtiene el ultimo insert de la secuencia insertada
+        /// </summary>
+        /// <returns>El numero mayor en la base de datos</returns>
         int ObtenerMaxSecuencia();
 
+
+        /// <summary>
+        /// Metodo que consulta las MarcaBaseTercero de una MarcaTercero
+        /// </summary>
+        /// <param name="marcaBaseTercero">MarcaBaseTercero con los Datos de MarcaTercero</param>
+        /// <returns>Todas las MarcaBaseTercero de la MarcaTercero</returns>
         List<MarcaBaseTercero> ObtenerTodosPorId(MarcaBaseTercero marcaBaseTercero);
 
-    //    Marca ObtenerMarcaTerceroConTodo(MarcaTercero marcaTercero);
+    
     }
 }
