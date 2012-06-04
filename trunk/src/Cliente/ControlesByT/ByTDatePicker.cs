c330e07e5d67d5c701c01e8a1b335bede94b2df2
danijose24 <danijose24@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2012-06-04 23:19:13 +0000

    Implementación del filtro de patente, implementacion de control para escribir la fecha a mano

---
+++ trunk/src/Cliente/ControlesByT/ByTDatePicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using System;
+
+namespace Trascend.Bolet.ControlesByT
+{
+    public class ByTDatePicker : System.Windows.Controls.DatePicker
+    {
+        public static readonly DependencyProperty EditableProperty = DependencyProperty.Register("Editable", typeof(bool),
+            typeof(ByTDatePicker), new PropertyMetadata(true));
+        public bool Editable
+        {
+            get { return (bool)GetValue(EditableProperty); }
+            set { SetValue(EditableProperty, value); }
+        }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var textBox = GetTemplateChild("PART_TextBox") as System.Windows.Controls.Primitives.DatePickerTextBox;
+            var binding = new Binding { Source = this, Path = new PropertyPath(ByTDatePicker.EditableProperty) };
+            textBox.SetBinding(System.Windows.Controls.Primitives.DatePickerTextBox.FocusableProperty, binding);
+        }
+
+
+        protected override void OnDateValidationError(System.Windows.Controls.DatePickerDateValidationErrorEventArgs e)
+        {
+
+            this.Text = null;
+
+            base.OnDateValidationError(e);  
+        }
+    }
+}
