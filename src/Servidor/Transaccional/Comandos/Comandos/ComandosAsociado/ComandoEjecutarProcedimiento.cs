b142afbb7da85f3b06202b7a8cf37cb152fc3f24
dirimo.quintero@gmail.com <dirimo.quintero@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2013-05-28 14:58:05 +0000

    Clase ComandoEjecutarProcedimiento en los usuarios, se usara para ejecutar el Package de Conectividad. Esto no esta completo. Falta completarlo.
    Version BoletSys V1.U

---
+++ src/Servidor/Transaccional/Comandos/Comandos/ComandosAsociado/ComandoEjecutarProcedimiento.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using NLog;
+using Trascend.Bolet.AccesoDatos.Contrato;
+using Trascend.Bolet.AccesoDatos.Fabrica;
+using Trascend.Bolet.ObjetosComunes.Entidades;
+
+namespace Trascend.Bolet.Comandos.Comandos.ComandosAsociado
+{
+    public class ComandoEjecutarProcedimiento: ComandoBase<bool>
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private ParametroProcedimiento _parametro;
+
+        /// <summary>
+        /// Constructor predeterminado
+        /// </summary>
+        /// <param name="parametro">ParametrosProcedimiento a Ejecutar</param>
+        public ComandoEjecutarProcedimiento(ParametroProcedimiento parametro)
+        {
+            this._parametro = parametro;
+        }
+
+        /// <summary>
+        /// Método que ejecuta el comando
+        /// </summary>
+        public override void Ejecutar()
+        {
+            try
+            {
+                #region trace
+                if (ConfigurationManager.AppSettings["Ambiente"].ToString().Equals("Desarrollo"))
+                    logger.Debug("Entrando al Método {0}", (new System.Diagnostics.StackFrame()).GetMethod().Name);
+                #endregion
+
+                IDaoAsociado dao = FabricaDaoBase.ObtenerFabricaDao().ObtenerDaoAsociado();
+                this.Receptor = new Receptor<bool>(dao.EjecutarProcedimiento(this._parametro));
+
+                #region trace
+                if (ConfigurationManager.AppSettings["Ambiente"].ToString().Equals("Desarrollo"))
+                    logger.Debug("Saliendo del Método {0}", (new System.Diagnostics.StackFrame()).GetMethod().Name);
+                #endregion
+            }
+            catch (ApplicationException ex)
+            {
+                logger.Error(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
