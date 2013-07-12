a6a79ed68737bbae0c0bc5c71706552eb2e10bcf
dirimo.quintero@gmail.com <dirimo.quintero@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2013-07-12 15:09:02 +0000

    Version V1.AR Objetos Comunes NOTA IMPORTANTE: ESTA VERSION DEBE ACTUALIZARSE AL COMPILAR CORRECTAMENTE

--- src/Comun/ObjetosComunes/ContratosServicios/ICertificadoMarcaServicios.cs
+++ src/Comun/ObjetosComunes/ContratosServicios/ICertificadoMarcaServicios.cs
@@ -5,6 +5,11 @@ namespace Trascend.Bolet.ObjetosComunes.ContratosServicios
 {
     public interface ICertificadoMarcaServicios : IServicioBase<CertificadoMarca>
     {
-
+        /// <summary>
+        /// Servicio que se encarga de consultar la auditoria de Certificados de Marca
+        /// </summary>
+        /// <param name="auditoria">Auditoria a consultar</param>
+        /// <returns>Lista de auditoria de la marca</returns>
+        IList<Auditoria> AuditoriaPorFkyTabla(Auditoria auditoria);
     }
 }
