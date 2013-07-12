81535e4e4324df9f349143076d9d3f865e68a61d
dirimo.quintero@gmail.com <dirimo.quintero@gmail.com@21b4276b-ba79-5147-a9a6-a99b7f46763f>
2013-07-12 15:17:16 +0000

    Actualzacion de las ventanas del Cliente V1.AR

---
+++ src/Cliente/Cliente/Ventanas/Marcas/GestionarFechaDeMarca.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Trascend.Bolet.Cliente.Contratos.Marcas;
+using Trascend.Bolet.Cliente.Presentadores.Marcas;
+using System.ComponentModel;
+using System.Threading;
+using Trascend.Bolet.Cliente.Ayuda;
+
+namespace Trascend.Bolet.Cliente.Ventanas.Marcas
+{
+    /// <summary>
+    /// Lógica de interacción para GestionarFechaDeMarca.xaml
+    /// </summary>
+    public partial class GestionarFechaDeMarca : Page, IGestionarFechaDeMarca
+    {
+
+        private GridViewColumnHeader _CurSortCol = null;
+        private SortAdorner _CurAdorner = null;
+        private PresentadorGestionarFechaDeMarca _presentador;
+        private bool _cargada;
+        BackgroundWorker _bgw = new BackgroundWorker();
+        
+        #region Constructores
+
+        public GestionarFechaDeMarca(object fechaMarca, object ventanaPadre)
+        {
+            InitializeComponent();
+            this._cargada = false;
+            this._presentador = new PresentadorGestionarFechaDeMarca(this, fechaMarca, ventanaPadre);
+
+            
+            _bgw.WorkerReportsProgress = true;
+            _bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(bgw_DoWork);
+            _bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
+            _bgw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(bgw_ProgressChanged);
+           
+        }
+
+        public GestionarFechaDeMarca(object fechaMarca, object ventanaPadre, object ventanaPadreLista)
+        {
+            InitializeComponent();
+            this._cargada = false;
+            this._presentador = new PresentadorGestionarFechaDeMarca(this, fechaMarca, ventanaPadre, ventanaPadreLista);
+
+
+            _bgw.WorkerReportsProgress = true;
+            _bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(bgw_DoWork);
+            _bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(bgw_RunWorkerCompleted);
+            _bgw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(bgw_ProgressChanged);
+
+        } 
+
+        #endregion
+
+
+
+        #region IGestionarFechaDeMarca
+
+        public bool EstaCargada
+        {
+            get { return this._cargada; }
+            set { this._cargada = value; }
+        }
+
+        public object FechaMarca
+        {
+            get { return this._gridDatos.DataContext; }
+            set { this._gridDatos.DataContext = value; }
+        }
+
+        public object TiposDeFechas
+        {
+            get { return this._cbxTipoFecha.DataContext; }
+            set { this._cbxTipoFecha.DataContext = value; }
+        }
+
+        public object TipoDeFecha
+        {
+            get { return this._cbxTipoFecha.SelectedItem; }
+            set { this._cbxTipoFecha.SelectedItem = value; }
+        }
+
+        public string IdCorrespondencia
+        {
+            get { return this._txtCorrespondencia.Text; }
+            set { this._txtCorrespondencia.Text = value; }
+        }
+
+        public void FocoPredeterminado()
+        {
+            this._btnAceptar.Focus();
+        }
+
+        #endregion
+
+
+
+        #region Eventos
+
+        void bgw_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            _bgw.ReportProgress(1);
+            Thread.Sleep(2000);
+            
+        }
+
+        void bgw_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            this._txtMensaje.Text = "Operacion realizada exitosamente";
+        }
+
+        void bgw_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            //this._presentador.irListaInfoBol();
+            this._presentador.irListaFechasDeMarca();
+        }
+        
+        
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!EstaCargada)
+            {
+                this._presentador.CargarPagina();
+                EstaCargada = true;
+            }
+        }
+
+
+        private void _btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            if (this._presentador.Aceptar())
+            {
+                _bgw.RunWorkerAsync();
+            }
+        }
+
+
+        private void _btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBoxResult.Yes == MessageBox.Show(Recursos.MensajesConElUsuario.ConfirmacionEliminarFechaMarca,
+                "Eliminar Fecha de Marca", MessageBoxButton.YesNo, MessageBoxImage.Question))
+            {
+                if (this._presentador.Eliminar())
+                {
+                    _bgw.RunWorkerAsync();
+                }
+            }
+        }
+
+
+        private void _btnRegresar_Click(object sender, RoutedEventArgs e)
+        {
+            this._presentador.RegresarVentanaPadre();
+        }
+        
+        #endregion
+
+        public void Mensaje(string mensaje, int opcion)
+        {
+            if (opcion == 0)
+                MessageBox.Show(mensaje, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+                MessageBox.Show(mensaje, "Advertencia", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        
+
+        
+    }
+}
---
+++ src/Cliente/Cliente/Ventanas/Marcas/ListaFechasDeMarca.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Trascend.Bolet.Cliente.Ayuda;
+using Trascend.Bolet.Cliente.Contratos.Marcas;
+using Trascend.Bolet.Cliente.Presentadores.Marcas;
+using Trascend.Bolet.ObjetosComunes.Entidades;
+
+namespace Trascend.Bolet.Cliente.Ventanas.Marcas
+{
+    /// <summary>
+    /// Lógica de interacción para ListaFechasDeMarca.xaml
+    /// </summary>
+    public partial class ListaFechasDeMarca : Page, IListaFechasDeMarca
+    {
+        private GridViewColumnHeader _CurSortCol = null;
+        private SortAdorner _CurAdorner = null;
+        private PresentadorListaFechasDeMarca _presentador;
+        private bool _cargada;
+        private object _marca;
+
+
+        #region IListaFechasDeMarca
+        public bool EstaCargada
+        {
+            get { return this._cargada; }
+            set { this._cargada = value; }
+        }
+
+        public void FocoPredeterminado()
+        {
+            this._btnRegresar.Focus();
+        }
+
+        public object Fechas
+        {
+            get { return this._lstResultados.DataContext; }
+            set { this._lstResultados.DataContext = value; }
+        }
+
+        public object FechaSeleccionada
+        {
+            get { return this._lstResultados.SelectedItem; }
+        }
+
+        
+        public GridViewColumnHeader CurSortCol
+        {
+            get { return _CurSortCol; }
+            set { _CurSortCol = value; }
+        }
+
+        public SortAdorner CurAdorner
+        {
+            get { return _CurAdorner; }
+            set { _CurAdorner = value; }
+        }
+
+
+        public ListView ListaResultados
+        {
+            get { return this._lstResultados; }
+            set { this._lstResultados = value; }
+        }
+
+        public string TotalHits
+        {
+            set { this._lblHits.Text = value; }
+        } 
+        #endregion
+        
+        
+        #region Constructores
+        
+        /// <summary>
+        /// Constructor predeterminado que recibe una marca
+        /// </summary>
+        /// <param name="marca"></param>
+        public ListaFechasDeMarca(object marca)
+        {
+            InitializeComponent();
+            
+            this._cargada = false;
+
+            this._presentador = new PresentadorListaFechasDeMarca(this, marca);
+        }
+
+
+        /// <summary>
+        /// Constructor predeterminado que recibe una marca y una ventana padre
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="ventanaPadre"></param>
+        public ListaFechasDeMarca(object marca, object ventanaPadre)
+        {
+            InitializeComponent();
+            this._cargada = false;
+            this._presentador = new PresentadorListaFechasDeMarca(this, marca, ventanaPadre);
+        }
+
+
+        #endregion
+
+        #region Eventos
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!EstaCargada)
+            {
+                this._presentador.CargarPagina();
+                EstaCargada = true;
+            }
+        }
+
+        private void _btnRegresar_Click(object sender, RoutedEventArgs e)
+        {
+            this._presentador.RegresarVentanaPadre();
+        }
+
+        private void EventoIrGestionarFechaDeMarca(object sender, EventArgs e)
+        {
+            if (sender.GetType().ToString().Equals("System.Windows.Controls.Button"))
+                this._presentador.IrGestionarFechaDeMarca(true);
+                //this._presentador.IrGestionarInfoBol(true);
+            else
+                this._presentador.IrGestionarFechaDeMarca(false);
+                //this._presentador.IrGestionarInfoBol(false);
+        }
+
+       
+        #endregion
+
+        
+
+        
+    }
+}
